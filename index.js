4f76906ddbaf3e5463eb71f08453f280fc187a3e
justin <justin.reynaert@gmail.com>
2017-10-18 23:26:51 +0200

    process.env

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const port = 5000;
+
 app.use(express.static(__dirname))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(5000, function() {
+app.listen(process.env.PORT || port, function() {
   console.log("App started on port 5000");
 })
