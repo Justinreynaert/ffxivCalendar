436a56de5646a540e9138c641e2bf92e5343590d
justin <justin.reynaert@gmail.com>
2017-10-22 21:00:51 +0200

    t

--- models/users.js
+++ models/users.js
@@ -50,6 +50,6 @@ module.exports.comparePassword = (candidatePassword, hash, callback) => {
     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
         if (err) throw err;
 
-        callback(null, isMatch);
+        callback(err, isMatch);
     });
 };
